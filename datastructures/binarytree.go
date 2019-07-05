013487f15e3741c8fe0fa832d99f1494a94dbc39
Mustafa Abuelfadl <mustafa.abuelfadl@tvsquared.com>
2019-07-05 23:13:01 +0200

    binarytree init
    Signed-off-by: Mustafa Abuelfadl <mustafa.abuelfadl@tvsquared.com>

---
+++ datastructures/binarytree.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+type TreeNode struct {
+	Val   interface{}
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func (t *TreeNode) Add(item interface{}) {
+
+}
+
+//Traverse
+//BFS
+//DFS
+//Greedy
+//A*
