6f5abf5d76031ae349ad044809b52d994f90c541
Mustafa Zidan <mustafa@zidan.me>
2019-07-07 12:22:59 +0200

    DFS recursive
    Signed-off-by: Mustafa Zidan <mustafa@zidan.me>

--- search/dfs.go
+++ search/dfs.go
@@ -23,6 +23,27 @@ func (node *ds.TreeNode) DFS(item interface{}) *ds.TreeNode{
 	return nil
 }
 
+
+// DFS recursion impel
+
+func( n * ds.TreeNode) DFSR(item interface{}) *ds.TreeNode {
+	if n.Val == item {
+		return n
+	}
+	var result *ds.TreeNode
+
+	if n.Left != nil {
+		result = n.Left.DFSR(item)
+	}
+
+	if n.Right != nil && result == nil {
+		result = n.Right.DFSR(item)
+	}
+
+	return result
+}
+
+
 func (node *ds.Node) DFS(item interface{}) *ds.Node{
 	stack := ds.NewStack()
 	stack.Push(n)
@@ -36,3 +57,4 @@ func (node *ds.Node) DFS(item interface{}) *ds.Node{
 	}
 	return nil
 }
+
