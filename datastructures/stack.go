3281c8edb819f7d22266371371444c3db4c8fca4
Mustafa Abuelfadl <mustafa.abuelfadl@damedic.ai>
2025-02-02 13:45:34 +0100

    chore(formatting): lint datastructure files
    Signed-off-by: Mustafa Abuelfadl <mustafa.abuelfadl@damedic.ai>

--- datastructures/stack.go
+++ datastructures/stack.go
@@ -1,45 +1,45 @@
 package datastructures
 
 import (
-    "sync"
+	"sync"
 )
 
 // Stack
 type Stack struct {
-    items []interface{}
-    lock  sync.RWMutex
+	items []interface{}
+	lock  sync.RWMutex
 }
 
-//New returns new instance pointer of Stack struct
+// NewStack  returns new instance pointer of Stack struct
 func NewStack() *Stack {
-    s := &Stack{}
-    s.items = make([]interface{}, 0)
-    return s
+	s := &Stack{}
+	s.items = make([]interface{}, 0)
+	return s
 }
 
 // Push adds an Item to the top of the stack
 func (s *Stack) Push(item interface{}) {
-    s.lock.Lock()
-    s.items = append(s.items, item)
-    s.lock.Unlock()
+	s.lock.Lock()
+	s.items = append(s.items, item)
+	s.lock.Unlock()
 }
 
 // Pop removes an Item from the top of the stack
 func (s *Stack) Pop() interface{} {
-    s.lock.Lock()
-    if len(s.items) == 0 {
-        return nil
-    }
-    item := s.items[len(s.items)-1]
-    s.items = s.items[:len(s.items)-1]
-    s.lock.Unlock()
-    return item
+	s.lock.Lock()
+	if len(s.items) == 0 {
+		return nil
+	}
+	item := s.items[len(s.items)-1]
+	s.items = s.items[:len(s.items)-1]
+	s.lock.Unlock()
+	return item
 }
 
 func (s *Stack) Len() int {
-    return len(s.items)
+	return len(s.items)
 }
 
 func (s *Stack) IsEmpty() bool {
-    return len(s.items) == 0
+	return len(s.items) == 0
 }
